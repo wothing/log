6253645264828cf35be828d5109ad50d8be7c175
Yongzheng Lai <sdrzlyz@126.com>
2016-06-12 12:00:38 +0800

    Revert "using printStructs in debug,info,error log"
    This reverts commit 6c58e537b8a377ab89f88d4f5c6bfccf469fa09d.

--- tracelog.go
+++ tracelog.go
@@ -1,9 +1,7 @@
 package log
 
 import (
-	"bytes"
 	"fmt"
-	"reflect"
 	"time"
 
 	"golang.org/x/net/context"
@@ -21,7 +19,7 @@ import (
 
 func TraceIn(traceID string, service string, format string, v ...interface{}) (string, string, time.Time) {
 	startTime := time.Now()
-	Std.Output(traceID, Linfo, 2, fmt.Sprintf("calling "+service+", "+format, printStructs(v)...))
+	Std.Output(traceID, Linfo, 2, fmt.Sprintf("calling "+service+", "+format, v...))
 	return traceID, service, startTime
 }
 
@@ -40,57 +38,4 @@ func TraceCtx(ctx context.Context, service string, format string, v ...interface
 	startTime := time.Now()
 	Std.Output(traceID, Linfo, 2, fmt.Sprintf("calling "+service+", "+format, v...))
 	return traceID, service, startTime
-}
-
-func printStructs(ss []interface{}) []interface{} {
-	for i, _ := range ss {
-		ss[i] = printStruct(ss[i], true)
-	}
-	return ss
-}
-
-func printStruct(s interface{}, names bool) string {
-	b := &bytes.Buffer{}
-	v := reflect.ValueOf(s)
-	if v.Kind() == reflect.Ptr {
-		b.WriteString("&")
-		v = reflect.Indirect(v)
-	}
-
-	t := v.Type()
-
-	if t.Kind() != reflect.Struct {
-		return fmt.Sprint(s)
-	}
-
-	b.WriteString(v.Type().String())
-
-	b.WriteString("{")
-	for i, l := 0, v.NumField(); i < l; i++ {
-		if i > 0 {
-			b.WriteString(" ")
-		}
-
-		if names {
-			b.WriteString(t.Field(i).Name)
-			b.WriteString(":")
-		}
-
-		v2 := v.Field(i)
-
-		switch v2.Kind() {
-		case reflect.Ptr, reflect.Interface:
-			if v2.Elem().IsValid() {
-				b.WriteString(printStruct(v2.Interface(), names))
-			} else {
-				b.WriteString("nil")
-			}
-		case reflect.Struct:
-			b.WriteString(printStruct(v2.Interface(), names))
-		default:
-			b.WriteString(fmt.Sprint(v2))
-		}
-	}
-	b.WriteString("}")
-	return b.String()
 }
